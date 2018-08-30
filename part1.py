707151e6a1295d287fc6a25e36c8534ebec26d83
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-08-30 18:56:57 -0500

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,7 +1,9 @@
 import random
 
 def guessTheNumber():
+    # Generate random number between 0 and 10
     number = random.randint(0, 10)
+    # Get user's guess
     guess = input("Guess a number between 0 and 10: ")
 
     if int(guess) < number:
